bcc918709740b7608a59a7717c38eacd88e76605
Alexandre Storelli <a_misc@storelli.fr>
2018-11-12 21:55:39 +0100

    do not use a service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-var isCordovaApp = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
-
-if (!isCordovaApp) {
-	registerServiceWorker();
-}
-
-/*window["isUpdateAvailable"].then(isAvailable => {
-	if (isAvailable) {
-		console.log("refresh to get new content");
-	}
-});*/
